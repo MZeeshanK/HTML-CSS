7dd3d59250dae9f532d06615c503d3ee847d4ed0
MZeeshanK <malikzeeshankhurshid@gmail.com>
2022-11-20 19:08:19 +0530

    Dynamically added projects using github api.

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const section = document.querySelector('#projects');
 
 const fetchProjects = async () => {
-  // const res = await fetch(
-  //   `https://api.github.com/repos/MZeeshanK/HTML_&_CSS/contents/projects`
-  // );
   const res = await fetch(
-    `https://api.github.com/repos/MZeeshanK/portfolio/contents/`
+    `https://api.github.com/repos/MZeeshanK/HTML-CSS/contents/projects`,
+    {
+      headers: {
+        Authorization: 'token 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',
+      },
+    }
   );
 
   const response = await res.json();
-
-  console.log(response);
-
-  let projects;
 
   response.map((res) => {
     const project = document.createElement('div');
@@ -20,8 +18,10 @@ const fetchProjects = async () => {
     project.classList.add('project-container');
 
     project.innerHTML = `
+    <a href='projects/${res.name}/index.html'>
     <h3>${res.name}</h3>
-    <a href='projects/Dropdown/index.html'><i class="fa-solid fa-square-arrow-up-right"></i></a>
+    <i class="fa-solid fa-square-arrow-up-right"></i>
+    </a>
     `;
 
     section.appendChild(project);
